4e51ff7084dfff3b420d88d71b17fd027d98674f
bibisakina907-bit <bibisakina907@gmail.com>
2026-06-29 11:06:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+VERIFY_TOKEN = "my_verify_token"
+
 @app.route("/")
 def home():
-    return "Bot is running on Render!"
+    return "Messenger Bot is running!"
+
+@app.route("/webhook", methods=["GET"])
+def verify():
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return challenge
+    return "Verification failed", 403
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json()
+    print(data)
+
+    return "ok", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
